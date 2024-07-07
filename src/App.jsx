d4e5a13f7fa43468096ed3963b5e625f87c8e1a6
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-07-07 00:07:51 -0400

    added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import projectsData from './projects.json';
-import tags_dict from './tags_dict.jsx';
-import ProjectItem from './ProjectItem.jsx';
-import TagSelect from './TagSelect.jsx';
-import Tag from './Tag.jsx';
-import Snowfall from './Snowfall.jsx';
-import github_logo from './assets/github_logo.svg';
-import itchio_logo from './assets/itchio_logo.svg';
-import devpost_logo from './assets/devpost_logo.svg';
-import clouds_texture from './assets/clouds.png';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Header.jsx';
+import Home from './Home.jsx';
 
 function App() {
-
-  const [projects, setProjects] = useState([]);
 
   const [scrollY, setScrollY] = useState(0);
   const [viewWidth, setViewWidth] = useState(0);
   const [viewHeight, setViewHeight] = useState(0);
 
-  const tagWidth = 200;
-
-  const minColumnWidth = 400;
-
-  const remainingWidth = viewWidth - tagWidth;
-
-  const numColumns = Math.floor(remainingWidth / minColumnWidth);
-
-  const projectsContainerRef = useRef(null);
-
-  const cloudsScale = viewHeight*15/100/600;
-
-  const cloudsArr = Array.from({ length: (viewWidth/(cloudsScale*2500) + 1) }, (_, index) => (
-    <img src={clouds_texture} style={{width: cloudsScale*2500, height: cloudsScale*600}}/>
-  ));
-
   useEffect(() => {
-    setProjects(projectsData.projects);
 
     const handleScroll = () => {
       setScrollY(window.scrollY);
@@ -59,63 +33,15 @@ function App() {
     };
 
   }, [scrollY]);
-
-  const [selectedTags, setSelectedTags] = useState(Object.keys(tags_dict));
-
-  const handleCheckboxChange = (event) => {
-    const { value, checked } = event.target;
-    if (checked) {
-      setSelectedTags(prevItems => [...prevItems, value]);
-    } else {
-      setSelectedTags(prevItems => prevItems.filter(item => item !== value));
-    }
-  };
-
-  const showProject = (project) => {
-    for (const tag of project.tags) {
-      if (selectedTags.includes(tag)) {
-        return <ProjectItem project={project}/>;
-      }
-    }
-    return;
-  };
   
   return (
     <div className="App" style={{display: "flex", flexDirection: "column", justifyContent: "center", overflow: "hidden"}}>
-      <div className="header" style={{display:"flex", position:"absolute", top: 0, left: 0, width: "100vw", height: "15vh"}}>
-        {cloudsArr}
-        <div>
+      <Header viewHeight={viewHeight} viewWidth={viewWidth} />
 
-        </div>
-      </div>
-      <div style={{position:"absolute", zIndex: -10, margin:0, padding:0, height:document.body.scrollHeight+10000, top: 0, left: 0}}>
-        <Snowfall height={document.body.scrollHeight+10000}/>
-      </div>
-
-      <div className="all-content" style={{position:"absolute", top: 0, left: 0}}>
-        <div className="title" style={{height:"100vh", textAlign:"left", color:"white", margin:"100px", marginTop:"180px", textShadow: "1px 1px 0px rgba(0, 0, 0, 0.5)"}}>
-          <h1>Hi!</h1>
-          <h1>I'm Sofia Egan</h1>
-          <p style={{width:"40vw"}}>I'm a high schooler in the Boston area. I go to a lot of hackathons and I ran my own this spring, called <a href="https://hacknight.co">HacKnight</a>! <br></br>I enjoy working with a diverse range of platforms and technologies, including apps, websites, games, and random ML and data analysis projects in Python.</p>
-          <div>
-            <a href="https://github.com/EerierGosling"> <img className="social-link" src={github_logo}></img> </a>
-            <a href="https://eeriergosling.itch.io/"> <img className="social-link" src={itchio_logo}></img> </a>
-            <a href=" https://devpost.com/segan"> <img className="social-link" src={devpost_logo}></img> </a>
-          </div>
-        </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       
-        <div className="tag-select-container" style={{ marginTop: `${Math.max(0, scrollY - (projectsContainerRef.current ? projectsContainerRef.current.offsetTop : 0))}px`, zIndex: 1, marginLeft:"30px", width: tagWidth}}>
-          <h2>Select Tags</h2>
-          <ul>
-            {Object.keys(tags_dict).map((tag, index) => (
-              <li key={index}><input type="checkbox" value={tag} onChange={handleCheckboxChange} defaultChecked /> <Tag key={index} tag={tags_dict[tag]}/> </li>
-            ))}
-          </ul>
-        </div>
-        <div className="projects-container" ref={projectsContainerRef} style={{gridTemplateColumns: `repeat(${numColumns}, 1fr)`}}>
-          {projects.map((project, index) => showProject(project))}
-        </div>
-      </div>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Home.css';
+import projectsData from './projects.json';
+import tags_dict from './tags_dict.jsx';
+import ProjectItem from './ProjectItem.jsx';
+import TagSelect from './TagSelect.jsx';
+import Tag from './Tag.jsx';
+import Snowfall from './Snowfall.jsx';
+import github_logo from './assets/github_logo.svg';
+import itchio_logo from './assets/itchio_logo.svg';
+import devpost_logo from './assets/devpost_logo.svg';
+import Header from './Header.jsx';
+
+function Home() {
+
+  const [projects, setProjects] = useState([]);
+
+  const [scrollY, setScrollY] = useState(0);
+  const [viewWidth, setViewWidth] = useState(0);
+  const [viewHeight, setViewHeight] = useState(0);
+
+  const tagWidth = 200;
+
+  const minColumnWidth = 400;
+
+  const remainingWidth = viewWidth - tagWidth;
+
+  const numColumns = Math.floor(remainingWidth / minColumnWidth);
+
+  const projectsContainerRef = useRef(null);
+
+  useEffect(() => {
+    setProjects(projectsData.projects);
+
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    const handleResize = () => {
+      setViewWidth(window.innerWidth);
+      setViewHeight(window.innerHeight);
+    };
+
+    handleScroll();
+    handleResize();
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('rezize', handleResize);
+    };
+
+  }, [scrollY]);
+
+  const [selectedTags, setSelectedTags] = useState(Object.keys(tags_dict));
+
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setSelectedTags(prevItems => [...prevItems, value]);
+    } else {
+      setSelectedTags(prevItems => prevItems.filter(item => item !== value));
+    }
+  };
+
+  const showProject = (project) => {
+    for (const tag of project.tags) {
+      if (selectedTags.includes(tag)) {
+        return <ProjectItem project={project}/>;
+      }
+    }
+    return;
+  };
+  
+  return (
+    <div className="Home" style={{display: "flex", flexDirection: "column", justifyContent: "center", overflow: "hidden"}}>
+      <Header viewHeight={viewHeight} viewWidth={viewWidth} />
+      <div style={{position:"absolute", zIndex: -10, margin:0, padding:0, height:document.body.scrollHeight+10000, top: 0, left: 0}}>
+        <Snowfall height={document.body.scrollHeight+10000}/>
+      </div>
+
+      <div className="all-content" style={{position:"absolute", top: 0, left: 0}}>
+        <div className="title" style={{height:"100vh", textAlign:"left", color:"white", margin:"100px", marginTop:"180px", textShadow: "1px 1px 0px rgba(0, 0, 0, 0.5)"}}>
+          <h1>Hi!</h1>
+          <h1>I'm Sofia Egan</h1>
+          <p style={{width:"40vw"}}>I'm a high schooler in the Boston area. I go to a lot of hackathons and I ran my own this spring, called <a href="https://hacknight.co">HacKnight</a>! <br></br>I enjoy working with a diverse range of platforms and technologies, including apps, websites, games, and random ML and data analysis projects in Python.</p>
+          <div>
+            <a href="https://github.com/EerierGosling"> <img className="social-link" src={github_logo}></img> </a>
+            <a href="https://eeriergosling.itch.io/"> <img className="social-link" src={itchio_logo}></img> </a>
+            <a href=" https://devpost.com/segan"> <img className="social-link" src={devpost_logo}></img> </a>
+          </div>
+        </div>
+      
+        <div className="tag-select-container" style={{ marginTop: `${Math.max(0, scrollY - (projectsContainerRef.current ? projectsContainerRef.current.offsetTop : 0))}px`, zIndex: 1, marginLeft:"30px", width: tagWidth}}>
+          <h2>Select Tags</h2>
+          <ul>
+            {Object.keys(tags_dict).map((tag, index) => (
+              <li key={index}><input type="checkbox" value={tag} onChange={handleCheckboxChange} defaultChecked /> <Tag key={index} tag={tags_dict[tag]}/> </li>
+            ))}
+          </ul>
+        </div>
+        <div className="projects-container" ref={projectsContainerRef} style={{gridTemplateColumns: `repeat(${numColumns}, 1fr)`}}>
+          {projects.map((project, index) => showProject(project))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
