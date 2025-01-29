ebda87c9374900a548049d1f461a537f8cd94462
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-01-29 17:52:48 -0500

    renaming file

---
+++ app/components/tags_dict.js
@@ -0,0 +1,22 @@
+const tags_dict = {
+  "Python": { name: "Python", color: "#0a568c" },
+  "JavaScript": { name: "JavaScript", color: "#f1e05a" },
+  "Swift": { name: "Swift", color: "#f5602f" },
+  "Arduino": { name: "Arduino", color: "#079ba3" }, // fix colors past here
+  "React": { name: "React", color: "#57d7fa" },
+  "Svelte": { name: "Svelte", color: "#fa4728" },
+  "Unity": { name: "Unity", color: "#575757" },
+  "HTML": { name: "HTML", color: "#e3470e" },
+  "CSS": { name: "CSS", color: "#4246d4" },
+  "Rhino 7": { name: "Rhino 7", color: "#7d7d7d" },
+  "Laser Cutting": { name: "Laser Cutting", color: "#bf1300" },
+  "OpenAI API" : { name: "OpenAI API", color: "#00bf8c" },
+  "C#": { name: "C#", color: "#8d46b0" },
+  "C++": { name: "C++", color: "#4e96de" },
+  "Raspberry Pi" : { name: "Raspberry Pi", color: "#BC1B48" },
+  "ESP32" : { name: "ESP32", color: "#505050" },
+  "Flask" : { name: "Flask", color: "#38A8BE" },
+  "Hugging Face" : { name: "Hugging Face", color: "#FFD21F" },
+}
+
+export default tags_dict;
--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,11 @@ export default function Home() {
           <h1>Hey!</h1>
           <h1>I'm Sofia Egan</h1>
           <h2 style={{width:"50vw"}}>My username is EerierGosling - you might see that around on my profiles!</h2>
-          <p style={{width:"50vw"}}>I'm a high schooler in the Boston area. I go to a lot of hackathons and I ran my own this spring, called <a href="https://hacknight.co">HacKnight</a>! <br></br>I enjoy working with a diverse range of platforms and technologies, including apps, websites, games, and random ML and data analysis projects in Python.</p>
+          <p style={{width:"50vw"}}>
+            I'm a high schooler in the Boston area.
+            I go to a lot of hackathons and I've run some of my own! I ran <a href="https://hacknight.co/">HacKnight</a> and was an organizer for <a href="https://counterspell.hackclub.com">Counterspell</a> globally and <a href="https://counterspell.hackclub.com/boston">Counterspell Boston</a>.
+            I enjoy working with a diverse range of platforms and technologies, including apps, websites, games, and random ML and data analysis projects in Python.
+          </p>
           <div>
             <a href="https://github.com/EerierGosling">
               <Image className="social-link" src={github_logo} alt="GitHub Logo" width={50} height={50} />
