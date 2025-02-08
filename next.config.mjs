ab31a5892993afa23ac72ae2a6089c44ecbacb2f
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-02-07 19:01:13 -0500

    adding redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/cubesat-presentation",
+        destination: "https://docs.google.com/presentation/d/1-KQB6vig9reWJ8C5WMaW6gTXZRoUWKWcors94sDVdIQ",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
