ae5da43d6cb0cb17b332f86d90e101e0cb5abade
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-02-07 19:12:17 -0500

    new redirect

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         destination: "https://docs.google.com/presentation/d/1-KQB6vig9reWJ8C5WMaW6gTXZRoUWKWcors94sDVdIQ",
         permanent: true,
       },
+      {
+        source: "/quantum-rsa-presentation",
+        destination: "https://docs.google.com/presentation/d/1wKFjC_F4-g37oC_v8BwO6wepv-TqtUJ8_eQu_beWUc4",
+        permanent: true,
+      },
     ];
   },
 };
