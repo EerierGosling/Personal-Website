1495932541d64da8dc9fd0fdac9a74f0599a080f
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-03-15 23:30:45 -0400

    adding file

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,16 @@ const nextConfig = {
         destination: "https://docs.google.com/presentation/d/1wKFjC_F4-g37oC_v8BwO6wepv-TqtUJ8_eQu_beWUc4",
         permanent: true,
       },
+      {
+        source: "/synbio-presentation",
+        destination: "https://docs.google.com/presentation/d/1uFHiXj-6oQ-ZH27bfJ4rbpKyVz8MTGwrC0slM-h6TUE",
+        permanent: true,
+      },
+      {
+        source: "/cnn-presentation",
+        destination: "https://math.mit.edu/research/highschool/primes/materials/2024/December/2-5-Egan.pdf",
+        permanent: true,
+      },
     ];
   },
 };
